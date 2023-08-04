8879d9af2f28e158b6bc9d250191904acefa5e4b
Vrindavan Sanap <vrindavansanap@gmail.com>
2023-08-04 09:30:09 +0530

    added chess peices

---
+++ chess_peices.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+import collections
+from random import choice
+
+Peice = collections.namedtuple("Peice", ["color", "name"])
+
+class Chess_set:
+    colors = ["White", "Black"]
+    names = ['King', 'Queen', 'Rook', 'Bishop', 'Knight', 'Pawn']
+
+    def __init__(self):
+        self._peices = [Peice(c, n) for c in self.colors for n in self.names]
+    
+    def __len__(self):
+        return len(self._peices)
+    
+    def __getitem__(self, position):
+        return self._peices[position]
+
+def rank_peice(peice):
+    return Chess_set.names.index(peice.name) +  Chess_set.colors.index(peice.color) * 8
+black_rook = Peice("Black", "Rook")
+white_pawn = Peice("White", "Pawn")
+
+a = rank_peice(white_pawn)
+b = rank_peice(black_rook)
+print(a, b)
+peices = Chess_set()
+print(sorted(peices,key = rank_peice) )
