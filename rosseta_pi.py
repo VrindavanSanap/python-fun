9d8e610d287c5876327dcfb84cde67004dc76b92
Vrindavan Sanap <vrindavansanap@gmail.com>
2023-08-24 11:04:29 +0530

    clean up

--- rosseta_pi.py
+++ rosseta_pi.py
@@ -1,21 +1,21 @@
 def calcPi(n_digits = 100):
     q, r, t, k, n, l = 1, 0, 1, 1, 3, 3
     for _ in range(n_digits):
-        if 4*q+r-t < n*t:
+        if (4*q + r - t) < n*t:
             yield n
-            nr = 10*(r-n*t)
-            n  = ((10*(3*q+r))//t)-10*n
-            q  *= 10
-            r  = nr
+            nr = 10*(r - n*t)
+            n = ((10*(3*q + r)) // t) - 10*n
+            q *= 10
+            r = nr
         else:
-            nr = (2*q+r)*l
-            nn = (q*(7*k)+2+(r*l))//(t*l)
-            q  *= k
-            t  *= l
-            l  += 2
+            nr = (2*q + r) * l
+            nn = (q*(7*k) + 2 + (r*l)) // (t*l)
+            q *= k
+            t *= l
+            l += 2
             k += 1
-            n  = nn
-            r  = nr
+            n = nn
+            r = nr
 
 import sys
 pi_digits = calcPi()
