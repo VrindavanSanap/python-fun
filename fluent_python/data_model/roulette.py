d702be39723edc40988f3f63edb5148e4a72c19c
Vrindavan Sanap <vrindavansanap@gmail.com>
2024-08-18 21:33:19 +0530

    -

--- fluent_python/data_model/roulette.py
+++ fluent_python/data_model/roulette.py
@@ -7,11 +7,11 @@ Roulette_number = namedtuple("R_num", "number color")
 
 
 class Roulette:
-  COLORS = "red black green".split()
-  NUMBERS = "0-28-9-26-30-11-7-20-32-17-5-22-34-15-3-24-36-13-1-00-27-10-25-29-12-8-19-31-18-6-21-33-16-4-23-35-14-2".split("-")
+	COLORS = "red black green".split()
+	NUMBERS = "0-28-9-26-30-11-7-20-32-17-5-22-34-15-3-24-36-13-1-00-27-10-25-29-12-8-19-31-18-6-21-33-16-4-23-35-14-2".split("-")
 
-  def __init__(self):
-    self.nums = [
+	def __init__(self):
+		self.nums = [
       Roulette_number(num, "green")
       if num in self.NUMBERS[::19]
       else Roulette_number(num, "red")
