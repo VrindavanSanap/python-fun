9613e52f66fb37ad87e2068f9bb84ab9c6960b7f
Vrindavan Sanap <vrindavansanap@gmail.com>
2024-08-19 19:58:36 +0530

    -

---
+++ network/call_api.py
@@ -0,0 +1,26 @@
+import requests
+import time
+
+BASE = "https://dummyjson.com/quotes"
+END_POINT = "random"
+
+def call_api(base, endpoint):
+  resp = requests.get(f"{base}/{endpoint}")
+  if resp.status_code != 200:
+    raise requests.HTTPError(f"HTTP error occurred: {resp.status_code} - {resp.reason}")
+  else:
+    return resp.json()
+
+def get_random_quote():
+  resp_json = call_api(BASE, END_POINT)
+  return resp_json["quote"]
+
+while True:
+  st = time.time()
+  quote = get_random_quote()
+  print(f"Quote: {quote}")
+  et = time.time()
+  time_taken = et - st
+  print(f"Got quote in {time_taken:.4f} seconds")
+
+
--- wrapper.py
+++ wrapper.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 def add_wrapping(item):
 	def wrapped_item():
 		return f"A wrapped up box of {item()}"
